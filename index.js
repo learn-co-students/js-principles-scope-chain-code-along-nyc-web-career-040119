2371f31d39908161cfb0725c242d573b77ae26a0
Daviicode <d.ydzdavid@yahoo.com>
2019-05-12 13:22:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+
+var customerName = 'bob';
+// var favoriteCustomer = 'someone';
+// const leastFavoriteCustomer = "david";
+
+ function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+ function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+ function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "you";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Not you';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'something is not right';
+}
