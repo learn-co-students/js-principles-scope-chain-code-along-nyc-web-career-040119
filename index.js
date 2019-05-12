4e2639d82932c0339c7207890e8d14556f0755b3
ferryrules <ferris@ferryrules.com>
2019-05-12 17:19:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-/* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bobert';
+
+function upperCaseCustomerName() {
+  customerName =  customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+};
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+};
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'ferris';
+};
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'anya';
+  let favoriteCustomer = 'abby';
+}
