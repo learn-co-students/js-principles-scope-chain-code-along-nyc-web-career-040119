9e20866e84da2c1369a075eddaa9b07c015831dd
Edgar Ong <emailedgar@gmail.com>
2019-05-12 09:48:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-/* Your code goes here */
+var customerName = 'bob'
+const leastFavoriteCustomer = 'edgar'
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'not edgar'
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'edgar'
+  // let favoriteCustomer = 'not edgar'
+  throw SyntaxError('this lab is broken')
+}
