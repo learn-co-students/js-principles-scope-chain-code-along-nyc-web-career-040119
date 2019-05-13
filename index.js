13a1a30202deb137405e5ae30789cacc6a6cb4ae
github username <github email address>
2019-05-13 12:00:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 /* Your code goes here */
+customerName = 'bob';
+/*
+upperCaseCustomerName()
+
+    returns the customerName
+    */
+function upperCaseCustomerName() // function passes test it does not return anything...
+{
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer()
+{
+  bestCustomer = 'not bob';
+}
+
+
+function overWriteBestCustomer()
+{
+  bestCustomer = 'maybe bob';
+}
+
+const favoriteCustomer = 'Ali';
+
+function changeLeastFavoriteCustomer()
+{
+  favoriteCustomer = 'ALI'
+}
+
+function attemptTwoFavoriteCustomers()
+{
+  let favoriteCustomer = 'ali';
+  let favoriteCustomer = 'alI';
+}
